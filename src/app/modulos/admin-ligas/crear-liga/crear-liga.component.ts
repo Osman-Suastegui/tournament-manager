4058229bb6d7d156e3af0c2fbb2c149a0b233490
JuanNajera02 <jpnajeracastro@hotmail.com>
2023-10-18 22:52:45 -0700

    se implemente la logica para crear una liga

--- src/app/modulos/admin-ligas/crear-liga/crear-liga.component.ts
+++ src/app/modulos/admin-ligas/crear-liga/crear-liga.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Liga } from '../../../models/Ligas/Ligas';
+import { LigasServiceService } from '../../../services/ligasService/ligas-service.service';
 
 @Component({
   selector: 'app-crear-liga',
@@ -6,9 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-liga.component.css']
 })
 export class CrearLigaComponent {
-
-
-  crearLiga(){
-    console.log("Liga creada");
+  liga: Liga = {
+    nombre: ''
   }
+
+  resul: any = "";
+  mensaje: string = "";
+
+
+  constructor(private ligaService: LigasServiceService) {}
+
+  crearLiga() {
+
+    this.ligaService.createLiga(this.liga).subscribe({
+      next: () => {
+        // Manejo de respuesta exitosa
+        console.log('Liga creada exitosamente.');
+        this.mensaje = 'Liga creada exitosamente.';
+      },
+      error: (error) => {
+        this.resul = error;
+        if(this.resul.status === 200){
+          console.log('Liga creada exitosamente.');
+          this.mensaje = 'Liga creada exitosamente.';
+        }else {
+          console.log(this.resul.error[0].message);
+          this.mensaje = this.resul.error[0].message;
+        }
+      }
+    });
+  }
+
 }
