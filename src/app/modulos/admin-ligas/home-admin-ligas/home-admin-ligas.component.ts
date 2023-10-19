4abce3d381ce5a6f9e20211d0b40072e64832783
JuanNajera02 <jpnajeracastro@hotmail.com>
2023-10-18 19:02:59 -0700

    se creo el diseño para crear una liga

--- src/app/modulos/admin-ligas/home-admin-ligas/home-admin-ligas.component.ts
+++ src/app/modulos/admin-ligas/home-admin-ligas/home-admin-ligas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CrearLigaComponent } from '../crear-liga/crear-liga.component';
 
 @Component({
   selector: 'app-home-admin-ligas',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-admin-ligas.component.css']
 })
 export class HomeAdminLigasComponent {
+  constructor(public dialog: MatDialog) { }
 
+  openDialog(): void {
+    this.dialog.open(CrearLigaComponent,{
+      width: '250px',
+    })
+  }
 }
