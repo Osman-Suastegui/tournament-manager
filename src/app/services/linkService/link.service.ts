16312ace6645cfb9757c4c023d244382bcee00f8
Osman <osman_suastegui@hotmail.com>
2025-01-15 21:30:12 -0700

    add design for link-modal

--- src/app/services/linkService/link.service.ts
+++ src/app/services/linkService/link.service.ts
@@ -3,14 +3,17 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { url } from 'src/app/url-config';
 
+export interface LinkResponse {
+  token: string
+}
 @Injectable({
   providedIn: 'root'
 })
 export class LinkService {
   private http = inject(HttpClient);
 
-  getLink(teamId:string,tournamentId:string):Observable<string>{
-    return this.http.get<string>(`${url}/team-tournament-links/getLink/${teamId}/${tournamentId}`);
+  getLink(teamId: string, tournamentId: string): Observable<LinkResponse> {
+    return this.http.get<LinkResponse>(`${url}/team-tournament-links/getLink/${teamId}/${tournamentId}`);
   }
 
 }
