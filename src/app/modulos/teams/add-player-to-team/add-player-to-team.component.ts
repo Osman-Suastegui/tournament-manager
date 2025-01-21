1b04b84a23e0e68a2ac7ab52eafeed47eda18859
Osman <osman_suastegui@hotmail.com>
2025-01-20 21:54:42 -0700

    show players and change design in add player modal

--- src/app/modulos/teams/add-player-to-team/add-player-to-team.component.ts
+++ src/app/modulos/teams/add-player-to-team/add-player-to-team.component.ts
@@ -1,16 +1,17 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { TeamService } from '../teamService/team.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormGroup } from '@angular/forms';
 import { AddPlayerToTeamForm } from '../interfaces';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Player } from '../../jugadores/interface';
 
 @Component({
   selector: 'app-add-player-to-team',
   templateUrl: './add-player-to-team.component.html',
   styleUrls: ['./add-player-to-team.component.css']
 })
-export class AddPlayerToTeamComponent {
+export class AddPlayerToTeamComponent implements OnInit {
 
   constructor(
     private teamServ: TeamService,
@@ -18,23 +19,35 @@ export class AddPlayerToTeamComponent {
     private dialogRef: MatDialogRef<AddPlayerToTeamComponent>
   ) { }
 
-  public addPlayerToTeamForm: FormGroup<AddPlayerToTeamForm> = this.teamServ.createAddPlayerToTeamForm()
+  ngOnInit(): void {
+    this.getPlayers()
+  }
 
-  addPlayerToTeam(teamId: string,tournamentId:string): void {
+  public addPlayerToTeamForm: FormGroup<AddPlayerToTeamForm> = this.teamServ.createAddPlayerToTeamForm()
+  public players: Player[] = []
+
+  addPlayerToTeam(teamId: string, tournamentId: string): void {
     if (this.addPlayerToTeamForm.invalid) {
       return
     }
-    const name  = this.addPlayerToTeamForm.value.name as string
-    console.log("name",name)
-    this.teamServ.addPlayerToTeamInTournament(tournamentId,teamId,name).subscribe({
+    const name = this.addPlayerToTeamForm.value.name as string
+    this.teamServ.addPlayerToTeamInTournament(tournamentId, teamId, name).subscribe({
       next: (res) => {
-        console.log("res",res)
         this.closeModal()
       },
-      error: (err:HttpErrorResponse) => {
-        console.log("err",err)
+      error: (err: HttpErrorResponse) => {
+        console.log("err", err)
       }
     })
+  }
+
+  getPlayers() {
+    this.teamServ.getPlayersInTournamentTeam(this.data.tournamentId, this.data.teamId).subscribe({
+      next: (players: Player[]) => {
+        this.players = players
+      }
+    })
+
   }
 
   closeModal() {
