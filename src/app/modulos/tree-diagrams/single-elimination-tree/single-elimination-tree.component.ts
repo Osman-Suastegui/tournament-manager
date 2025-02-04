551dc92608c3a8d2ee583f66038383244df19c38
Osman <osman_suastegui@hotmail.com>
2025-02-03 20:08:51 -0700

    add styles changes to tree component

--- src/app/modulos/tree-diagrams/single-elimination-tree/single-elimination-tree.component.ts
+++ src/app/modulos/tree-diagrams/single-elimination-tree/single-elimination-tree.component.ts
@@ -1,8 +1,9 @@
-import { AfterViewInit, Component, OnInit, ViewEncapsulation, ViewChild, Renderer2, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, Renderer2, ElementRef } from "@angular/core";
 class TreeTeamNode {
   public winner: "left" | "right" | null = null;
-  constructor(public name: string = '', public left?: TreeTeamNode, public right?: TreeTeamNode) {
-    this.name = name;
+  constructor(public teamName1: string = "", public teamName2: string = "", public left?: TreeTeamNode, public right?: TreeTeamNode) {
+    this.teamName1 = teamName1;
+    this.teamName2 = teamName2;
     this.left = left;
     this.right = right;
   }
@@ -12,203 +13,179 @@ class TreeTeamNode {
 class Tree {
   static inorder(node?: TreeTeamNode) {
     if (!node) return;
-    console.log(node.name);
+    console.log(node.teamName1 + " vs " + node.teamName2);
     this.inorder(node.left);
     this.inorder(node.right);
+  }
+
+  static getDepth(node: TreeTeamNode | undefined): number {
+    if (!node) return 0;
+    return 1 + this.getDepth(node.left);
   }
 }
 
 @Component({
-  selector: 'app-single-elimination-tree',
-  templateUrl: './single-elimination-tree.component.html',
-  styleUrls: ['./single-elimination-tree.component.css'],
+  selector: "app-single-elimination-tree",
+  templateUrl: "./single-elimination-tree.component.html",
+  styleUrls: ["./single-elimination-tree.component.css"],
 
 })
 export class SingleEliminationTreeComponent implements OnInit, AfterViewInit {
-  @ViewChild('tree', { static: false }) tree!: ElementRef;
+  @ViewChild("tree", { static: false }) tree!: ElementRef;
   constructor(private renderer: Renderer2) { }
 
+  private WIDTH_NODE = 153;
+  private HEIGHT_NODE = 32;
   ngOnInit(): void {
   }
+
   ngAfterViewInit(): void {
-    const root: TreeTeamNode = new TreeTeamNode(
-      "Team1",
+    const tree1 = new TreeTeamNode(
+      "", "",
       new TreeTeamNode(
-        "Team2",
+        "", "",
+        new TreeTeamNode("Team A", "Team B"),
+        new TreeTeamNode("Team C", "Team D")
+      ),
+      new TreeTeamNode(
+        "", "",
+        new TreeTeamNode("Team E", "Team F"),
+        new TreeTeamNode("Team G", "Team H")
+      )
+    );
+    const tree2 = new TreeTeamNode(
+      '', '',
+      new TreeTeamNode(
+        'Team C', 'Team G',
         new TreeTeamNode(
-          "Team4",
-          new TreeTeamNode("Team8"),
-          new TreeTeamNode("Team9")
+          'Team A', 'Team C',
+          new TreeTeamNode('Team A', 'Team B'),
+          new TreeTeamNode('Team C', 'Team D')
         ),
         new TreeTeamNode(
-          "Team5",
-          new TreeTeamNode("Team10"),
-          new TreeTeamNode("Team11")
+          'Team E', 'Team G',
+          new TreeTeamNode('Team E', 'Team F'),
+          new TreeTeamNode('Team G', 'Team H')
         )
       ),
       new TreeTeamNode(
-        "Team3",
+        '', '',
         new TreeTeamNode(
-          "Team6",
-          new TreeTeamNode("Team12"),
-          new TreeTeamNode("Team13")
+          '', '',
+          new TreeTeamNode('Team I', 'Team J'),
+          new TreeTeamNode('Team K', 'Team L')
         ),
         new TreeTeamNode(
-          "Team7",
-          new TreeTeamNode("Team14"),
-          new TreeTeamNode("Team15")
+          '', '',
+          new TreeTeamNode('Team M', 'Team N'),
+          new TreeTeamNode('Team O', 'Team P')
         )
       )
     );
 
-    const root2: TreeTeamNode = new TreeTeamNode(
-      "Team1",
-      new TreeTeamNode(
-        "Team2",
-        new TreeTeamNode(
-          "Team4",
-          new TreeTeamNode(
-            "Team8",
-            new TreeTeamNode("Team16"),
-            new TreeTeamNode("Team17")
-          ),
-          new TreeTeamNode(
-            "Team9",
-            new TreeTeamNode("Team18"),
-            new TreeTeamNode("Team19")
-          )
-        ),
-        new TreeTeamNode(
-          "Team5",
-          new TreeTeamNode(
-            "Team10",
-            new TreeTeamNode("Team20"),
-            new TreeTeamNode("Team21")
-          ),
-          new TreeTeamNode(
-            "Team11",
-            new TreeTeamNode("Team22"),
-            new TreeTeamNode("Team23")
-          )
-        )
-      ),
-      new TreeTeamNode(
-        "Team3",
-        new TreeTeamNode(
-          "Team6",
-          new TreeTeamNode(
-            "Team12",
-            new TreeTeamNode("Team24"),
-            new TreeTeamNode("Team25")
-          ),
-          new TreeTeamNode(
-            "Team13",
-            new TreeTeamNode("Team26"),
-            new TreeTeamNode("Team27")
-          )
-        ),
-        new TreeTeamNode(
-          "Team7",
-          new TreeTeamNode(
-            "Team14",
-            new TreeTeamNode("Team28"),
-            new TreeTeamNode("Team29")
-          ),
-          new TreeTeamNode(
-            "Team15",
-            new TreeTeamNode("Team30"),
-            new TreeTeamNode("Team31")
-          )
-        )
-      )
-    );
-
-    Tree.inorder(root);
-    this.createTreeHtml(root2)
+    Tree.inorder(tree1);
+    this.createTreeHtml(tree2);
   }
 
-  clickNode(name:string){
-    console.log(name)
-    console.log('click')
+  onHover(teamName: string, isHovering: boolean) {
+    const nodes = document.querySelectorAll(`.tree-node-team-${teamName}`);
+
+    nodes.forEach((node) => {
+      if (isHovering) {
+        this.renderer.addClass(node, "active");  // Add hover effect
+      } else {
+        this.renderer.removeClass(node, "active");  // Remove hover effect
+      }
+    });
   }
-  createTreeHtml(node: TreeTeamNode | undefined, x: number = window.innerWidth / 1.5, y: number = 400): void {
-    if (!node) return;
-    const WIDTH_NODE = 153
+
+
+  createTreeHtml(root: TreeTeamNode | undefined, x: number = window.innerWidth / 2 , y: number = 400): void {
+    if (!root) return;
+    const TREE_DEPTH = Tree.getDepth(root);
     // espacio entre nodos horizontal
-    const SPACE_BETWEEN_NODES = 250;
-    let level = 1;
-    const queue: { node: TreeTeamNode, parent: TreeTeamNode | null, isLeft: boolean | null, x: number, y: number, diffY: number }[] = [
-      { node, parent: null, isLeft: null, x, y, diffY: 0 }
+    const SPACE_BETWEEN_NODES = 200;
+    let currLevel = 1;
+    const queue: { node: TreeTeamNode, isLeft: boolean | null, x: number, y: number, diffY: number }[] = [
+      { node: root, isLeft: null, x, y, diffY: 0 }
     ];
 
     while (queue.length > 0) {
       const l = queue.length;
 
       for (let i = 0; i < l; i++) {
-        const { node, parent, isLeft, x, y, diffY } = queue.shift()!;
-        const div = this.renderer.createElement('div');
+        const { node, isLeft, x, y, diffY } = queue.shift()!;
 
-        div.classList.add('tree-node-team');
-        div.style.top = `${y}px`;
-        div.style.width = `${WIDTH_NODE}px`;
-        div.innerHTML = node.name;
-        div.addEventListener('click', () => this.clickNode(node.name))
+        const divTeam1 = this.createTreeNodeHtml(node.teamName1 || " vs ", x, y, true);
+        const divTeam2 = this.createTreeNodeHtml(node.teamName2 || " vs ", x, y + this.HEIGHT_NODE, false);
 
-        let parentX = x;
-
-        if (parent) {
-          div.style.left = `${parentX}px`;
-        } else {
-          // Root node stays centered
-          div.style.left = `${x}px`;
-        }
-
-        const line = this.renderer.createElement('div');
-        line.classList.add('line');
-        line.style.top = `${50}%`
-        const lineWidth = Math.round((SPACE_BETWEEN_NODES - WIDTH_NODE) / 2)
+        const line = this.renderer.createElement("div");
+        line.classList.add("line");
+        line.style.top = `${100}%`;
+        const lineWidth = Math.round((SPACE_BETWEEN_NODES - this.WIDTH_NODE) / 2);
         line.style.width = `${lineWidth}px`;
         line.style.left = `${-lineWidth}px`;
-        this.renderer.appendChild(this.tree.nativeElement, div);
+        this.renderer.appendChild(this.tree.nativeElement, divTeam1);
 
-        if(level == 5){
-          line.style.width = `0px`;
+        // IF WE ARE NOT IN THE LAST LEVEL
+        if (TREE_DEPTH !== currLevel) {
+          this.renderer.appendChild(divTeam1, line);
         }
-        this.renderer.appendChild(div, line);
 
-        const line2 = this.renderer.createElement('div');
-        line2.classList.add('line');
-        line2.style.top = `${50}%`
+        const line2 = this.renderer.createElement("div");
+        line2.classList.add("line");
+        line2.style.top = `${100}%`;
         line2.style.width = `${lineWidth}px`;
         line2.style.right = `${-lineWidth}px`;
-        this.renderer.appendChild(this.tree.nativeElement, div);
+
+        this.renderer.appendChild(this.tree.nativeElement, divTeam1);
+        this.renderer.appendChild(this.tree.nativeElement, divTeam2);
 
         if (isLeft) {
           line2.style.height = `${diffY}px`;
         }
-        this.renderer.appendChild(div, line2);
-        let leftPosYNode = y - (180 / level);
-        let rightPosYNode = y + (180 / level);
-        if(level == 4){
-          leftPosYNode += 30
-          rightPosYNode -= 30
-        }
-        if(level == 3) {
-          leftPosYNode += 15
-          rightPosYNode -= 15
+
+        // IF WE ARE NOT IN THE FIRST LEVEL WE ADD THE LINE
+        if (currLevel !== 1) {
+          this.renderer.appendChild(divTeam1, line2);
         }
 
-        let diff = rightPosYNode - leftPosYNode;
+        const containerHeight = window.innerHeight;           // total height of the browser
+        const drawingHeight = containerHeight * 0.8;            // use 80% of the height for the tree
+        const availableSpace = drawingHeight / Math.pow(2, currLevel); // space available for this level's subtree
+        const offset = availableSpace / 2;                      // half the space for positioning children
+        const leftPosYNode = y - offset;
+        const rightPosYNode = y + offset;
+        const diff = rightPosYNode - leftPosYNode;
+
         // Add children to the queue
         if (node.left) {
-
-          queue.push({ node: node.left, parent: node, isLeft: true, x: parentX - SPACE_BETWEEN_NODES, y: leftPosYNode, diffY: diff });
+          queue.push({ node: node.left, isLeft: true, x: x - SPACE_BETWEEN_NODES, y: leftPosYNode, diffY: diff });
         }
         if (node.right) {
-          queue.push({ node: node.right, parent: node, isLeft: false, x: parentX - SPACE_BETWEEN_NODES, y: rightPosYNode, diffY: diff });
+          queue.push({ node: node.right, isLeft: false, x: x - SPACE_BETWEEN_NODES, y: rightPosYNode, diffY: diff });
         }
       }
-      level++;
+      currLevel++;
     }
+  }
+
+  createTreeNodeHtml(teamName: string, x: number, y: number, hasBorders: boolean): HTMLDivElement {
+    // remove spaces teamnAMNE
+    teamName = teamName.replace(/\s/g, "");
+    const div = this.renderer.createElement("div");
+    div.classList.add("tree-node-team", `tree-node-team-${teamName}`);
+
+    div.style.top = `${y}px`;
+    div.style.left = `${x}px`;
+    div.style.width = `${this.WIDTH_NODE}px`;
+    if (!hasBorders) {
+      div.style.borderTop = "none";
+    }
+    div.innerHTML = teamName;
+    this.renderer.listen(div, 'mouseover', () => this.onHover(teamName, true));  // Mouse enters
+    this.renderer.listen(div, 'mouseout', () => this.onHover(teamName, false));  // Mouse leaves
+    return div;
+
   }
 }
